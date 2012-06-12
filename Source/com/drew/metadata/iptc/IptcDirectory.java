61f1cc5cef808817772d986c699c3a6f5eab86f5
Drew Noakes <git@drewnoakes.com>
2012-06-12 22:22:05 +0100

    More consistent tag name.

--- Source/com/drew/metadata/iptc/IptcDirectory.java
+++ Source/com/drew/metadata/iptc/IptcDirectory.java
@@ -129,7 +129,7 @@ public class IptcDirectory extends Directory
         _tagNameMap.put(TAG_FILE_VERSION, "File Version");
         _tagNameMap.put(TAG_SERVICE_ID, "Service Identifier");
         _tagNameMap.put(TAG_ENVELOPE_NUMBER, "Envelope Number");
-        _tagNameMap.put(TAG_PRODUCT_ID, "Product ID");
+        _tagNameMap.put(TAG_PRODUCT_ID, "Product Identifier");
         _tagNameMap.put(TAG_ENVELOPE_PRIORITY, "Envelope Priority");
         _tagNameMap.put(TAG_DATE_SENT, "Date Sent");
         _tagNameMap.put(TAG_TIME_SENT, "Time Sent");
