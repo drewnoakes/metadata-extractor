cac725cb5f79b636017da716d36e96c981ce9663
Drew Noakes <git@drewnoakes.com>
2013-01-01 00:44:05 +0000

    Created PngHeader class for decoding PNG IHDR chunks.

---
+++ Source/com/drew/imaging/png/PngHeader.java
@@ -0,0 +1,77 @@
+package com.drew.imaging.png;
+
+import com.drew.lang.SequentialByteArrayReader;
+import com.drew.lang.SequentialReader;
+import com.drew.lang.annotations.NotNull;
+
+import java.io.IOException;
+
+/**
+ * @author Drew Noakes http://drewnoakes.com
+ */
+public class PngHeader
+{
+    private int _imageWidth;
+    private int _imageHeight;
+    private byte _bitsPerSample;
+    private PngColorType _colorType;
+    private byte _compressionType;
+    private byte _filterMethod;
+    private byte _interlaceMethod;
+
+    public PngHeader(@NotNull PngChunk chunk) throws PngProcessingException
+    {
+        byte[] bytes = chunk.getBytes();
+        if (bytes.length != 13) {
+            throw new PngProcessingException("PNG header chunk must have 13 data bytes");
+        }
+        SequentialReader reader = new SequentialByteArrayReader(bytes);
+        try {
+            _imageWidth = reader.getInt32();
+            _imageHeight = reader.getInt32();
+            _bitsPerSample = reader.getInt8();
+            _colorType = PngColorType.fromNumericValue(reader.getInt8());
+            _compressionType = reader.getInt8();
+            _filterMethod = reader.getInt8();
+            _interlaceMethod = reader.getInt8();
+        } catch (IOException e) {
+            // Should never happen
+            throw new PngProcessingException(e);
+        }
+    }
+
+    public int getImageWidth()
+    {
+        return _imageWidth;
+    }
+
+    public int getImageHeight()
+    {
+        return _imageHeight;
+    }
+
+    public byte getBitsPerSample()
+    {
+        return _bitsPerSample;
+    }
+
+    public PngColorType getColorType()
+    {
+        return _colorType;
+    }
+
+    public byte getCompressionType()
+    {
+        return _compressionType;
+    }
+
+    public byte getFilterMethod()
+    {
+        return _filterMethod;
+    }
+
+    public byte getInterlaceMethod()
+    {
+        return _interlaceMethod;
+    }
+}
