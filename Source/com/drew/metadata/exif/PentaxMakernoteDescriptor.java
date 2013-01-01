fa07a2e58ffd5530fde9e6b9627c1178885fb0e5
Drew Noakes <git@drewnoakes.com>
2013-01-01 22:00:13 +0000

    Simplified PentaxMakernoteDescriptor.

--- Source/com/drew/metadata/exif/PentaxMakernoteDescriptor.java
+++ Source/com/drew/metadata/exif/PentaxMakernoteDescriptor.java
@@ -25,7 +25,7 @@ import com.drew.lang.annotations.Nullable;
 import com.drew.metadata.TagDescriptor;
 
 /**
- * Provides human-readable string representations of tag values stored in a <code>PentaxMakernoteDirectory</code>.
+ * Provides human-readable string representations of tag values stored in a {@link PentaxMakernoteDirectory}.
  * <p/>
  * Some information about this makernote taken from here:
  * http://www.ozhiker.com/electronics/pjmt/jpeg_info/pentax_mn.html
@@ -42,8 +42,7 @@ public class PentaxMakernoteDescriptor extends TagDescriptor<PentaxMakernoteDire
     @Nullable
     public String getDescription(int tagType)
     {
-        switch (tagType) 
-        {
+        switch (tagType) {
             case PentaxMakernoteDirectory.TAG_CAPTURE_MODE:
                 return getCaptureModeDescription();
             case PentaxMakernoteDirectory.TAG_QUALITY_LEVEL:
@@ -74,29 +73,19 @@ public class PentaxMakernoteDescriptor extends TagDescriptor<PentaxMakernoteDire
     @Nullable
     public String getColourDescription()
     {
-        Integer value = _directory.getInteger(PentaxMakernoteDirectory.TAG_COLOUR);
-        if (value==null)
-            return null;
-        switch (value)
-        {
-            case 1:  return "Normal";
-            case 2:  return "Black & White";
-            case 3:  return "Sepia";
-            default: return "Unknown (" + value + ")";
-        }
+        return getIndexedDescription(PentaxMakernoteDirectory.TAG_COLOUR, 1, "Normal", "Black & White", "Sepia");
     }
 
     @Nullable
     public String getIsoSpeedDescription()
     {
         Integer value = _directory.getInteger(PentaxMakernoteDirectory.TAG_ISO_SPEED);
-        if (value==null)
+        if (value == null)
             return null;
-        switch (value)
-        {
+        switch (value) {
             // TODO there must be other values which aren't catered for here
-            case 10:  return "ISO 100";
-            case 16:  return "ISO 200";
+            case 10: return "ISO 100";
+            case 16: return "ISO 200";
             case 100: return "ISO 100";
             case 200: return "ISO 200";
             default: return "Unknown (" + value + ")";
@@ -106,55 +95,28 @@ public class PentaxMakernoteDescriptor extends TagDescriptor<PentaxMakernoteDire
     @Nullable
     public String getSaturationDescription()
     {
-        Integer value = _directory.getInteger(PentaxMakernoteDirectory.TAG_SATURATION);
-        if (value==null)
-            return null;
-        switch (value)
-        {
-            case 0:  return "Normal";
-            case 1:  return "Low";
-            case 2:  return "High";
-            default: return "Unknown (" + value + ")";
-        }
+        return getIndexedDescription(PentaxMakernoteDirectory.TAG_SATURATION, "Normal", "Low", "High");
     }
 
     @Nullable
     public String getContrastDescription()
     {
-        Integer value = _directory.getInteger(PentaxMakernoteDirectory.TAG_CONTRAST);
-        if (value==null)
-            return null;
-        switch (value)
-        {
-            case 0:  return "Normal";
-            case 1:  return "Low";
-            case 2:  return "High";
-            default: return "Unknown (" + value + ")";
-        }
+        return getIndexedDescription(PentaxMakernoteDirectory.TAG_CONTRAST, "Normal", "Low", "High");
     }
 
     @Nullable
     public String getSharpnessDescription()
     {
-        Integer value = _directory.getInteger(PentaxMakernoteDirectory.TAG_SHARPNESS);
-        if (value==null)
-            return null;
-        switch (value)
-        {
-            case 0:  return "Normal";
-            case 1:  return "Soft";
-            case 2:  return "Hard";
-            default: return "Unknown (" + value + ")";
-        }
+        return getIndexedDescription(PentaxMakernoteDirectory.TAG_SHARPNESS, "Normal", "Soft", "Hard");
     }
 
     @Nullable
     public String getDigitalZoomDescription()
     {
         Float value = _directory.getFloatObject(PentaxMakernoteDirectory.TAG_DIGITAL_ZOOM);
-        if (value==null)
+        if (value == null)
             return null;
-        if (value==0)
+        if (value == 0)
             return "Off";
         return Float.toString(value);
     }
@@ -162,121 +124,49 @@ public class PentaxMakernoteDescriptor extends TagDescriptor<PentaxMakernoteDire
     @Nullable
     public String getWhiteBalanceDescription()
     {
-        Integer value = _directory.getInteger(PentaxMakernoteDirectory.TAG_WHITE_BALANCE);
-        if (value==null)
-            return null;
-        switch (value)
-        {
-            case 0:  return "Auto";
-            case 1:  return "Daylight";
-            case 2:  return "Shade";
-            case 3:  return "Tungsten";
-            case 4:  return "Fluorescent";
-            case 5:  return "Manual";
-            default: return "Unknown (" + value + ")";
-        }
+        return getIndexedDescription(PentaxMakernoteDirectory.TAG_WHITE_BALANCE, "Auto", "Daylight", "Shade", "Tungsten", "Fluorescent", "Manual");
     }
 
     @Nullable
     public String getFlashModeDescription()
     {
-        Integer value = _directory.getInteger(PentaxMakernoteDirectory.TAG_FLASH_MODE);
-        if (value==null)
-            return null;
-        switch (value)
-        {
-            case 1:  return "Auto";
-            case 2:  return "Flash On";
-            case 4:  return "Flash Off";
-            case 6:  return "Red-eye Reduction";
-            default: return "Unknown (" + value + ")";
-        }
+        return getIndexedDescription(PentaxMakernoteDirectory.TAG_FLASH_MODE,
+            1, "Auto", "Flash On", null, "Flash Off", null, "Red-eye Reduction");
     }
 
     @Nullable
     public String getFocusModeDescription()
     {
-        Integer value = _directory.getInteger(PentaxMakernoteDirectory.TAG_FOCUS_MODE);
-        if (value==null)
-            return null;
-        switch (value)
-        {
-            case 2:  return "Custom";
-            case 3:  return "Auto";
-            default: return "Unknown (" + value + ")";
-        }
+        return getIndexedDescription(PentaxMakernoteDirectory.TAG_FOCUS_MODE, 2, "Custom", "Auto");
     }
 
     @Nullable
     public String getQualityLevelDescription()
     {
-        Integer value = _directory.getInteger(PentaxMakernoteDirectory.TAG_QUALITY_LEVEL);
-        if (value==null)
-            return null;
-        switch (value)
-        {
-            case 0:  return "Good";
-            case 1:  return "Better";
-            case 2:  return "Best";
-            default: return "Unknown (" + value + ")";
-        }
+        return getIndexedDescription(PentaxMakernoteDirectory.TAG_QUALITY_LEVEL, "Good", "Better", "Best");
     }
 
     @Nullable
     public String getCaptureModeDescription()
     {
-        Integer value = _directory.getInteger(PentaxMakernoteDirectory.TAG_CAPTURE_MODE);
-        if (value==null)
-            return null;
-        switch (value)
-        {
-            case 1:  return "Auto";
-            case 2:  return "Night-scene";
-            case 3:  return "Manual";
-            case 4:  return "Multiple";
-            default: return "Unknown (" + value + ")";
-        }
+        return getIndexedDescription(PentaxMakernoteDirectory.TAG_CAPTURE_MODE,
+            1, "Auto", "Night-scene", "Manual", "Multiple");
     }
 
 /*
     public String getPrintImageMatchingInfoDescription()
     {
-        byte[] bytes = _directory.getByteArray(PentaxMakernoteDirectory.TAG_PANASONIC_PRINT_IMAGE_MATCHING_INFO);
-        if (bytes==null)
-            return null;
-        return "(" + bytes.length + " bytes)";
+        return getByteLengthDescription(PentaxMakernoteDirectory.TAG_PANASONIC_PRINT_IMAGE_MATCHING_INFO);
     }
 
     public String getMacroModeDescription()
     {
-        Integer value = _directory.getInteger(PentaxMakernoteDirectory.TAG_PANASONIC_MACRO_MODE);
-        if (value==null)
-            return null;
-        switch (value) {
-            case 1:
-                return "On";
-            case 2:
-                return "Off";
-            default:
-                return "Unknown (" + value + ")";
-        }
+        return getIndexedDescription(PentaxMakernoteDirectory.TAG_PANASONIC_MACRO_MODE, 1, "On", "Off");
     }
 
     public String getRecordModeDescription()
     {
-        Integer value = _directory.getInteger(PentaxMakernoteDirectory.TAG_PANASONIC_RECORD_MODE);
-        if (value==null)
-            return null;
-        switch (value) {
-            case 1:
-                return "Normal";
-            case 2:
-                return "Portrait";
-            case 9:
-                return "Macro";
-            default:
-                return "Unknown (" + value + ")";
-        }
+        return getIndexedDescription(PentaxMakernoteDirectory.TAG_PANASONIC_RECORD_MODE, 1, "Normal", "Portrait", "Macro");
     }
 */
 }
