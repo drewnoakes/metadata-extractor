0a9d95340f97232356f9b97c95169b6a9d4a5ecb
Drew Noakes <git@drewnoakes.com>
2014-11-25 20:31:33 +0000

    Set time zone during test execution. Relates to #6.

--- Tests/com/drew/imaging/png/PngMetadataReaderTest.java
+++ Tests/com/drew/imaging/png/PngMetadataReaderTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.TimeZone;
 
 import static org.junit.Assert.*;
 
@@ -43,24 +44,34 @@ public class PngMetadataReaderTest
     @Test
     public void testGimpGreyscaleWithManyChunks() throws Exception
     {
-        PngDirectory directory = processFile("Tests/Data/gimp-8x12-greyscale-alpha-time-background.png", PngDirectory.class);
+        TimeZone timeZone = TimeZone.getDefault();
 
-        assertEquals(8, directory.getInt(PngDirectory.TAG_IMAGE_WIDTH));
-        assertEquals(12, directory.getInt(PngDirectory.TAG_IMAGE_HEIGHT));
-        assertEquals(8, directory.getInt(PngDirectory.TAG_BITS_PER_SAMPLE));
-        assertEquals(4, directory.getInt(PngDirectory.TAG_COLOR_TYPE));
-        assertEquals(0, directory.getInt(PngDirectory.TAG_COMPRESSION_TYPE));
-        assertEquals(0, directory.getInt(PngDirectory.TAG_FILTER_METHOD));
-        assertEquals(0, directory.getInt(PngDirectory.TAG_INTERLACE_METHOD));
-        assertEquals(0.45455, directory.getDouble(PngDirectory.TAG_GAMMA), 0.00001);
-        assertArrayEquals(new byte[]{0, 52}, directory.getByteArray(PngDirectory.TAG_BACKGROUND_COLOR));
-        //noinspection ConstantConditions
-        assertEquals("Tue Jan 01 04:08:30 GMT 2013", directory.getDate(PngDirectory.TAG_LAST_MODIFICATION_TIME).toString());
-        @SuppressWarnings("unchecked")
-        List<KeyValuePair> pairs = (List<KeyValuePair>)directory.getObject(PngDirectory.TAG_TEXTUAL_DATA);
-        assertNotNull(pairs);
-        assertEquals(1, pairs.size());
-        assertEquals("Comment", pairs.get(0).getKey());
-        assertEquals("Created with GIMP", pairs.get(0).getValue());
+        try {
+            TimeZone.setDefault(TimeZone.getTimeZone("GMT"));
+
+            PngDirectory directory = processFile("Tests/Data/gimp-8x12-greyscale-alpha-time-background.png", PngDirectory.class);
+
+            assertEquals(8, directory.getInt(PngDirectory.TAG_IMAGE_WIDTH));
+            assertEquals(12, directory.getInt(PngDirectory.TAG_IMAGE_HEIGHT));
+            assertEquals(8, directory.getInt(PngDirectory.TAG_BITS_PER_SAMPLE));
+            assertEquals(4, directory.getInt(PngDirectory.TAG_COLOR_TYPE));
+            assertEquals(0, directory.getInt(PngDirectory.TAG_COMPRESSION_TYPE));
+            assertEquals(0, directory.getInt(PngDirectory.TAG_FILTER_METHOD));
+            assertEquals(0, directory.getInt(PngDirectory.TAG_INTERLACE_METHOD));
+            assertEquals(0.45455, directory.getDouble(PngDirectory.TAG_GAMMA), 0.00001);
+            assertArrayEquals(new byte[]{0, 52}, directory.getByteArray(PngDirectory.TAG_BACKGROUND_COLOR));
+            //noinspection ConstantConditions
+            assertEquals("Tue Jan 01 04:08:30 GMT 2013", directory.getDate(PngDirectory.TAG_LAST_MODIFICATION_TIME).toString());
+            @SuppressWarnings("unchecked")
+            List<KeyValuePair> pairs = (List<KeyValuePair>)directory.getObject(PngDirectory.TAG_TEXTUAL_DATA);
+            assertNotNull(pairs);
+            assertEquals(1, pairs.size());
+            assertEquals("Comment", pairs.get(0).getKey());
+            assertEquals("Created with GIMP", pairs.get(0).getValue());
+        }
+        finally
+        {
+            TimeZone.setDefault(timeZone);
+        }
     }
 }
