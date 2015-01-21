6915f28db31661a184fb2193eaddd70cd0fb5626
Drew Noakes <git@drewnoakes.com>
2015-01-21 22:21:56 +0000

    Make file type detection more rigorous. Relates to #68.

--- Source/com/drew/imaging/FileTypeDetector.java
+++ Source/com/drew/imaging/FileTypeDetector.java
@@ -24,20 +24,22 @@ import com.drew.lang.annotations.NotNull;
 
 import java.io.BufferedInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 
 /**
  * Examines the a file's first bytes and estimates the file's type.
  */
 public class FileTypeDetector
 {
-    private static final int JPEG_FILE_MAGIC_NUMBER = 0xFFD8;
-    private static final int MOTOROLA_TIFF_MAGIC_NUMBER = 0x4D4D;  // "MM"
-    private static final int INTEL_TIFF_MAGIC_NUMBER = 0x4949;     // "II"
-    private static final int PSD_MAGIC_NUMBER = 0x3842;            // "8B" // TODO the full magic number is 8BPS
-    private static final int PNG_MAGIC_NUMBER = 0x8950;            // "?P" // TODO the full magic number is six bytes long
-    private static final int BMP_MAGIC_NUMBER = 0x424D;            // "BM" // TODO technically there are other very rare magic numbers for OS/2 BMP files...
-    private static final int GIF_MAGIC_NUMBER = 0x4749;            // "GI" // TODO the full magic number is GIF or possibly GIF89a/GIF87a
+    // https://en.wikipedia.org/wiki/List_of_file_signatures
+
+    private static final int  JPEG_MAGIC_NUMBER          = 0xFFD8;
+    private static final int  MOTOROLA_TIFF_MAGIC_NUMBER = 0x4D4D002A;   // "MM?*"
+    private static final int  INTEL_TIFF_MAGIC_NUMBER    = 0x49492A00;   // "II*?"
+    private static final int  PSD_MAGIC_NUMBER    = 0x38425053;          // "8BPS"
+    private static final long PNG_MAGIC_NUMBER    = 0x89504E470D0A1A0AL; // "?PNG????"
+    private static final int  BMP_MAGIC_NUMBER    = 0x424D;              // "BM" // TODO technically there are other very rare magic numbers for OS/2 BMP files...
+    private static final long GIF87a_MAGIC_NUMBER = 0x474946383761L;     // "GIF87a"
+    private static final long GIF89a_MAGIC_NUMBER = 0x474946383961L;     // "GIF89a"
 
     private FileTypeDetector() throws Exception
     {
@@ -50,59 +52,47 @@ public class FileTypeDetector
      * Requires a {@link BufferedInputStream} in order to mark and reset the stream to the position
      * at which it was provided to this method once completed.
      * <p>
-     * Requires the stream to contain at least two bytes.
+     * Requires the stream to contain at least eight bytes.
      *
      * @throws IOException if an IO error occurred or the input stream ended unexpectedly.
      */
     @NotNull
     public static FileType detectFileType(@NotNull final BufferedInputStream inputStream) throws IOException
     {
-        int magicNumber = peekMagicNumber(inputStream);
+        byte[] bytes = new byte[8];
 
-        if (magicNumber == -1)
+        inputStream.mark(8);
+
+        int bytesRead = inputStream.read(bytes);
+
+        inputStream.reset();
+
+        if (bytesRead != 8)
             throw new IOException("Stream ended before file's magic number could be determined.");
 
-        // This covers all JPEG files
-        if ((magicNumber & JPEG_FILE_MAGIC_NUMBER) == JPEG_FILE_MAGIC_NUMBER)
+        int  two   =              (bytes[0] & 0xFF) << 8 | (bytes[1] & 0xFF);
+        int  four  = two  << 16 | (bytes[2] & 0xFF) << 8 | (bytes[3] & 0xFF);
+        long six   = four << 16 | (bytes[4] & 0xFF) << 8 | (bytes[5] & 0xFF);
+        long eight = six  << 16 | (bytes[6] & 0xFF) << 8 | (bytes[7] & 0xFF);
+
+        if (two == JPEG_MAGIC_NUMBER)
             return FileType.Jpeg;
 
-        // This covers all TIFF and camera RAW files
-        if (magicNumber == INTEL_TIFF_MAGIC_NUMBER || magicNumber == MOTOROLA_TIFF_MAGIC_NUMBER)
+        if (two == INTEL_TIFF_MAGIC_NUMBER || two == MOTOROLA_TIFF_MAGIC_NUMBER)
             return FileType.Tiff;
 
-        // This covers PSD files
-        // TODO we should really check all 4 bytes of the PSD magic number
-        if (magicNumber == PSD_MAGIC_NUMBER)
-            return FileType.Psd;
-
-        // This covers PNG files
-        if (magicNumber == PNG_MAGIC_NUMBER)
-            return FileType.Png;
-
-        // This covers BMP files
-        if (magicNumber == BMP_MAGIC_NUMBER)
+        if (two == BMP_MAGIC_NUMBER)
             return FileType.Bmp;
 
-        // This covers GIF files
-        if (magicNumber == GIF_MAGIC_NUMBER)
+        if (four == PSD_MAGIC_NUMBER)
+            return FileType.Psd;
+
+        if (eight == PNG_MAGIC_NUMBER)
+            return FileType.Png;
+
+        if (six == GIF87a_MAGIC_NUMBER || six == GIF89a_MAGIC_NUMBER)
             return FileType.Gif;
 
         return FileType.Unknown;
-    }
-
-    /**
-     * Reads the first two bytes from <code>inputStream</code>, then rewinds.
-     */
-    private static int peekMagicNumber(@NotNull final InputStream inputStream) throws IOException
-    {
-        inputStream.mark(2);
-        final int byte1 = inputStream.read();
-        final int byte2 = inputStream.read();
-        inputStream.reset();
-
-        if (byte1 == -1 || byte2 == -1)
-            return -1;
-
-        return byte1 << 8 | byte2;
     }
 }
