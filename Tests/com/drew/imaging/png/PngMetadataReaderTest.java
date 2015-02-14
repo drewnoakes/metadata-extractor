e5c9f29e14f833b2f21889dc5561f9397b2f1cd1
Drew Noakes <git@drewnoakes.com>
2015-02-14 00:32:42 +0000

    Fix broken unit test.

--- Tests/com/drew/imaging/png/PngMetadataReaderTest.java
+++ Tests/com/drew/imaging/png/PngMetadataReaderTest.java
@@ -45,7 +45,7 @@ public class PngMetadataReaderTest
             Collection<PngDirectory> directories = metadata.getDirectoriesOfType(PngDirectory.class);
 
             assertNotNull(directories);
-            assertEquals(5, directories.size());
+            assertEquals(6, directories.size());
 
             PngDirectory[] dirs = new PngDirectory[directories.size()];
             directories.toArray(dirs);
@@ -66,12 +66,17 @@ public class PngMetadataReaderTest
             assertArrayEquals(new byte[]{0, 52}, dirs[2].getByteArray(PngDirectory.TAG_BACKGROUND_COLOR));
 
             //noinspection ConstantConditions
-            assertEquals(PngChunkType.tIME, dirs[3].getPngChunkType());
-            assertEquals("Tue Jan 01 04:08:30 GMT 2013", dirs[3].getDate(PngDirectory.TAG_LAST_MODIFICATION_TIME).toString());
+            assertEquals(PngChunkType.pHYs, dirs[3].getPngChunkType());
+            assertEquals(1, dirs[3].getInt(PngDirectory.TAG_UNIT_SPECIFIER));
+            assertEquals(2835, dirs[3].getInt(PngDirectory.TAG_PIXELS_PER_UNIT_X));
+            assertEquals(2835, dirs[3].getInt(PngDirectory.TAG_PIXELS_PER_UNIT_Y));
 
-            assertEquals(PngChunkType.iTXt, dirs[4].getPngChunkType());
+            assertEquals(PngChunkType.tIME, dirs[4].getPngChunkType());
+            assertEquals("Tue Jan 01 04:08:30 GMT 2013", dirs[4].getDate(PngDirectory.TAG_LAST_MODIFICATION_TIME).toString());
+
+            assertEquals(PngChunkType.iTXt, dirs[5].getPngChunkType());
             @SuppressWarnings("unchecked")
-            List<KeyValuePair> pairs = (List<KeyValuePair>)dirs[4].getObject(PngDirectory.TAG_TEXTUAL_DATA);
+            List<KeyValuePair> pairs = (List<KeyValuePair>)dirs[5].getObject(PngDirectory.TAG_TEXTUAL_DATA);
             assertNotNull(pairs);
             assertEquals(1, pairs.size());
             assertEquals("Comment", pairs.get(0).getKey());
