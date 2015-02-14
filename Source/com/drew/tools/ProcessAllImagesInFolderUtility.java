c36e62602fbf35c7fef535b3effb96edd5a6a7a2
Drew Noakes <git@drewnoakes.com>
2015-02-14 23:59:40 +0000

    ProcessAllImagesInFolderUtility includes AI, EPS and PCX file extensions.

--- Source/com/drew/tools/ProcessAllImagesInFolderUtility.java
+++ Source/com/drew/tools/ProcessAllImagesInFolderUtility.java
@@ -135,7 +135,7 @@ public class ProcessAllImagesInFolderUtility
     {
         private final Set<String> _supportedExtensions = new HashSet<String>(
             Arrays.asList(
-                "jpg", "jpeg", "png", "gif", "bmp", "ico", "webp",
+                "jpg", "jpeg", "png", "gif", "bmp", "ico", "webp", "pcx", "ai", "eps",
                 "nef", "crw", "cr2", "orf", "arw", "raf", "srw", "x3f", "rw2", "rwl",
                 "tif", "tiff", "psd", "dng"));
 
