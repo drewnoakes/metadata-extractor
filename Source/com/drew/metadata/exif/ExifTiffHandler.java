334143e6dc09a62a19b843ce909b8f7a70732bc1
Drew Noakes <git@drewnoakes.com>
2016-03-26 09:38:30 +0900

    Reformat.

--- Source/com/drew/metadata/exif/ExifTiffHandler.java
+++ Source/com/drew/metadata/exif/ExifTiffHandler.java
@@ -67,15 +67,23 @@ public class ExifTiffHandler extends DirectoryTiffHandler
 
     public boolean isTagIfdPointer(int tagId)
     {
-        if (tagId == ExifIFD0Directory.TAG_EXIF_SUB_IFD_OFFSET && _currentDirectory instanceof ExifIFD0Directory) {
-            pushDirectory(ExifSubIFDDirectory.class);
-            return true;
-        } else if (tagId == ExifIFD0Directory.TAG_GPS_INFO_OFFSET && _currentDirectory instanceof ExifIFD0Directory) {
-            pushDirectory(GpsDirectory.class);
-            return true;
-        } else if (tagId == ExifSubIFDDirectory.TAG_INTEROP_OFFSET && _currentDirectory instanceof ExifSubIFDDirectory) {
-            pushDirectory(ExifInteropDirectory.class);
-            return true;
+        if (_currentDirectory instanceof ExifIFD0Directory) {
+            if (tagId == ExifIFD0Directory.TAG_EXIF_SUB_IFD_OFFSET) {
+                pushDirectory(ExifSubIFDDirectory.class);
+                return true;
+            }
+
+            if (tagId == ExifIFD0Directory.TAG_GPS_INFO_OFFSET) {
+                pushDirectory(GpsDirectory.class);
+                return true;
+            }
+        }
+
+        if (_currentDirectory instanceof ExifSubIFDDirectory) {
+            if (tagId == ExifSubIFDDirectory.TAG_INTEROP_OFFSET) {
+                pushDirectory(ExifInteropDirectory.class);
+                return true;
+            }
         }
 
         return false;
