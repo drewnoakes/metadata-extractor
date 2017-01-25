5b07a49f7b3d90c43a36a79dc4f6474845e1ebc7
Drew Noakes <git@drewnoakes.com>
2017-01-25 22:19:49 +0000

    Remove problematic XMP code.
    It's not currently possible to reliably map XMP's namespace/path
    structure to metadata-extractor's integer-based tag identifier system.
    
    The current code attempts to map a few properties to tags, but users
    who rely on these commonly miss other valuable tags. This partial
    XMP support has been the cause of many issues over the years, and
    I've been considering removing this support for a long time.
    
    Long term, I want the library to allow each directory a means to identify
    their own tags. The integer system stems from starting with TIFF/Exif.
    Until that time, I think it's better to remove this code. Much of it is
    commented out and incomplete.
    
    Some users might be surprised to see these tags no longer exist,
    but I believe that by using the XMPMeta object they'll be better
    served in the long term.
    
    I can imagine providing some constants and helper methods for
    working with XMPCore, if needed.

--- Source/com/drew/metadata/xmp/XmpDescriptor.java
+++ Source/com/drew/metadata/xmp/XmpDescriptor.java
@@ -20,13 +20,8 @@
  */
 package com.drew.metadata.xmp;
 
-import com.drew.imaging.PhotographicConversions;
-import com.drew.lang.Rational;
 import com.drew.lang.annotations.NotNull;
-import com.drew.lang.annotations.Nullable;
 import com.drew.metadata.TagDescriptor;
-
-import static com.drew.metadata.xmp.XmpDirectory.*;
 
 /**
  * Contains all logic for the presentation of xmp data, as stored in Xmp-Segment.  Use
@@ -37,119 +32,8 @@ import static com.drew.metadata.xmp.XmpDirectory.*;
 @SuppressWarnings("WeakerAccess")
 public class XmpDescriptor extends TagDescriptor<XmpDirectory>
 {
-    // TODO some of these methods look similar to those found in Exif*Descriptor... extract common functionality from both
-
     public XmpDescriptor(@NotNull XmpDirectory directory)
     {
         super(directory);
-    }
-
-    /** Do some simple formatting, dependant upon tagType */
-    @Override
-    public String getDescription(int tagType)
-    {
-        switch (tagType) {
-            case TAG_MAKE:
-            case TAG_MODEL:
-                return _directory.getString(tagType);
-            case TAG_EXPOSURE_TIME:
-                return getExposureTimeDescription();
-            case TAG_EXPOSURE_PROGRAM:
-                return getExposureProgramDescription();
-            case TAG_SHUTTER_SPEED:
-                return getShutterSpeedDescription();
-            case TAG_F_NUMBER:
-                return getFNumberDescription();
-            case TAG_LENS:
-            case TAG_LENS_INFO:
-            case TAG_CAMERA_SERIAL_NUMBER:
-            case TAG_FIRMWARE:
-                return _directory.getString(tagType);
-            case TAG_FOCAL_LENGTH:
-                return getFocalLengthDescription();
-            case TAG_APERTURE_VALUE:
-                return getApertureValueDescription();
-            default:
-                return super.getDescription(tagType);
-        }
-    }
-
-    /** Do a simple formatting like ExifSubIFDDescriptor.java */
-    @Nullable
-    public String getExposureTimeDescription()
-    {
-        final String value = _directory.getString(TAG_EXPOSURE_TIME);
-        if (value==null)
-            return null;
-        return value + " sec";
-    }
-
-    /** This code is from ExifSubIFDDescriptor.java */
-    @Nullable
-    public String getExposureProgramDescription()
-    {
-        return getIndexedDescription(TAG_EXPOSURE_PROGRAM,
-            1,
-            "Manual control",
-            "Program normal",
-            "Aperture priority",
-            "Shutter priority",
-            "Program creative (slow program)",
-            "Program action (high-speed program)",
-            "Portrait mode",
-            "Landscape mode"
-        );
-    }
-
-
-    /** This code is from ExifSubIFDDescriptor.java */
-    @Nullable
-    public String getShutterSpeedDescription()
-    {
-        final Float value = _directory.getFloatObject(TAG_SHUTTER_SPEED);
-        if (value==null)
-            return null;
-
-        // thanks to Mark Edwards for spotting and patching a bug in the calculation of this
-        // description (spotted bug using a Canon EOS 300D)
-        // thanks also to Gli Blr for spotting this bug
-        if (value <= 1) {
-            float apexPower = (float) (1 / (Math.exp(value * Math.log(2))));
-            long apexPower10 = Math.round((double) apexPower * 10.0);
-            float fApexPower = (float) apexPower10 / 10.0f;
-            return fApexPower + " sec";
-        } else {
-            int apexPower = (int) ((Math.exp(value * Math.log(2))));
-            return "1/" + apexPower + " sec";
-        }
-    }
-
-    /** Do a simple formatting like ExifSubIFDDescriptor.java */
-    @Nullable
-    public String getFNumberDescription()
-    {
-        final Rational value = _directory.getRational(TAG_F_NUMBER);
-        if (value==null)
-            return null;
-        return getFStopDescription(value.doubleValue());
-    }
-
-    /** This code is from ExifSubIFDDescriptor.java */
-    @Nullable
-    public String getFocalLengthDescription()
-    {
-        final Rational value = _directory.getRational(TAG_FOCAL_LENGTH);
-        return value == null ? null : getFocalLengthDescription(value.doubleValue());
-    }
-
-    /** This code is from ExifSubIFDDescriptor.java */
-    @Nullable
-    public String getApertureValueDescription()
-    {
-        final Double value = _directory.getDoubleObject(TAG_APERTURE_VALUE);
-        if (value==null)
-            return null;
-        double fStop = PhotographicConversions.apertureToFStop(value);
-        return getFStopDescription(fStop);
     }
 }
