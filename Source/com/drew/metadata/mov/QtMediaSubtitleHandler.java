21fa537d3d8c9abc187ab4059538bccf3ab8420c
Payton Garland <payton.r.g@gmail.com>
2017-07-24 16:28:03 -0500

    Add shell of subtitle media handler

---
+++ Source/com/drew/metadata/mov/QtMediaSubtitleHandler.java
@@ -0,0 +1,33 @@
+package com.drew.metadata.mov;
+
+import com.drew.lang.ByteArrayReader;
+
+import java.io.IOException;
+
+public class QtMediaSubtitleHandler extends QtMediaHandler
+{
+    @Override
+    String getMediaInformation()
+    {
+        // Not yet implemented
+        return null;
+    }
+
+    @Override
+    void processSampleDescription(QtDirectory directory, ByteArrayReader reader) throws IOException
+    {
+        // Not yet implemented
+    }
+
+    @Override
+    void processMediaInformation(QtDirectory directory, ByteArrayReader reader) throws IOException
+    {
+        // Not yet implemented
+    }
+
+    @Override
+    void processTimeToSample(QtDirectory directory, ByteArrayReader reader) throws IOException
+    {
+        // Not yet implemented
+    }
+}
