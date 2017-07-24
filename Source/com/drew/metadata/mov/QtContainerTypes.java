a318b6804f9d5cfb7bfd6f62efc1d1815d1f7b2d
Payton Garland <payton.r.g@gmail.com>
2017-07-24 16:03:08 -0500

    Clean up type classes

--- Source/com/drew/metadata/mov/QtContainerTypes.java
+++ Source/com/drew/metadata/mov/QtContainerTypes.java
@@ -7,16 +7,15 @@ import java.util.ArrayList;
  */
 public class QtContainerTypes
 {
-    // Contains information that describes the movie's data
-    public static final String ATOM_MOVIE = "moov";
-    public static final String ATOM_USER_DATA = "udta";
-    public static final String ATOM_TRACK = "trak";
-    public static final String ATOM_MEDIA = "mdia";
-    public static final String ATOM_MEDIA_INFORMATION = "minf";
-    public static final String ATOM_SAMPLE_TABLE = "stbl";
-    public static final String ATOM_METADATA_LIST = "ilst";
-    public static final String ATOM_METADATA = "meta";
-    public static final String ATOM_COMPRESSED_MOVIE = "cmov";
+    public static final String ATOM_MOVIE                       = "moov";
+    public static final String ATOM_USER_DATA                   = "udta";
+    public static final String ATOM_TRACK                       = "trak";
+    public static final String ATOM_MEDIA                       = "mdia";
+    public static final String ATOM_MEDIA_INFORMATION           = "minf";
+    public static final String ATOM_SAMPLE_TABLE                = "stbl";
+    public static final String ATOM_METADATA_LIST               = "ilst";
+    public static final String ATOM_METADATA                    = "meta";
+    public static final String ATOM_COMPRESSED_MOVIE            = "cmov";
 
     public static ArrayList<String> _containerList = new ArrayList<String>();
 
