878e9a9071bdbf8c9fd4d11194fd8051fa3df62f
Payton Garland <payton.r.g@gmail.com>
2017-07-25 10:04:35 -0500

    Add sample description implementation for subtitle media

--- Source/com/drew/metadata/mov/QtMediaSubtitleHandler.java
+++ Source/com/drew/metadata/mov/QtMediaSubtitleHandler.java
@@ -16,7 +16,26 @@ public class QtMediaSubtitleHandler extends QtMediaHandler
     @Override
     void processSampleDescription(QtDirectory directory, ByteArrayReader reader) throws IOException
     {
-        // Not yet implemented
+        // Begin general structure
+        int versionAndFlags = reader.getInt32(0);
+        int numberOfEntries = reader.getInt32(4);
+        int sampleDescriptionSize = reader.getInt32(8);
+        String dataFormat = reader.getString(12, 4, "UTF-8");
+        // 6-bytes of reserved space set to 0
+        int dataReferenceIndex = reader.getInt16(22);
+        // End general structure
+
+        int displayFlags = reader.getInt32(24);
+        // 8-bits reserved space set to 1
+        // 8-bits reserved space set to -1
+        // 32-bits reserved space set to 0
+        long defaultTextBox = reader.getInt64(30);
+        // 32-bits reserved space set to 0
+        int fontIdentifier = reader.getInt16(42);
+        int fontFace = reader.getInt8(44);
+        int fontSize = reader.getInt8(45);
+        int foregroundColor = reader.getInt32(46);
+        // font table atom 'ftab' not currently parsed
     }
 
     @Override
