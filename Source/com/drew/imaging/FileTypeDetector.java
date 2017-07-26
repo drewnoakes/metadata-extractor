3d090398ef3d428dc5e57f2d0dd2cbfefff3dbde
Payton Garland <payton.r.g@gmail.com>
2017-07-26 10:11:22 -0500

    Clarify comments in filetypedetector and specify meaning of size 0 in reader

--- Source/com/drew/imaging/FileTypeDetector.java
+++ Source/com/drew/imaging/FileTypeDetector.java
@@ -73,13 +73,13 @@ public class FileTypeDetector
         _root.addPath(FileType.Orf, "IIRS".getBytes(), new byte[]{(byte)0x08, 0x00});
         _root.addPath(FileType.Raf, "FUJIFILMCCD-RAW".getBytes());
         _root.addPath(FileType.Rw2, "II".getBytes(), new byte[]{0x55, 0x00});
-
-        _root.addPath(FileType.Mov, new byte[]{0x6D, 0x6F, 0x6F, 0x76}); // MOOV
-        _root.addPath(FileType.Mov, new byte[]{0x66, 0x74, 0x79, 0x70}); // FTYP
-        _root.addPath(FileType.Mov, new byte[]{0x66, 0x74, 0x79, 0x70, 0x71, 0x74}); // FTYPQT
-        _root.addPath(FileType.Mov, new byte[]{0x77, 0x69, 0x64, 0x65}); // Wide
-        _root.addPath(FileType.Mov, new byte[]{0x6D, 0x64, 0x61, 0x74}); // Mdat
-        _root.addPath(FileType.Mov, new byte[]{0x66, 0x72, 0x65, 0x65}); // Free
+        
+        // Potential root atoms... typically starts with FTYP
+        _root.addPath(FileType.Mov, new byte[]{0x6D, 0x6F, 0x6F, 0x76}); // moov
+        _root.addPath(FileType.Mov, new byte[]{0x66, 0x74, 0x79, 0x70}); // ftyp
+        _root.addPath(FileType.Mov, new byte[]{0x77, 0x69, 0x64, 0x65}); // wide
+        _root.addPath(FileType.Mov, new byte[]{0x6D, 0x64, 0x61, 0x74}); // mdat
+        _root.addPath(FileType.Mov, new byte[]{0x66, 0x72, 0x65, 0x65}); // free
 
     }
 
