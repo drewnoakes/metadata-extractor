882437f4ae1cba78457f9b4964b326ed24e503c3
Payton Garland <payton.r.g@gmail.com>
2017-08-01 14:05:31 -0500

    Add package-info for boxes and site ISO standards

---
+++ Source/com/drew/metadata/mp4/boxes/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Boxes are formed as per the official ISO base media file format specifications
+ *
+ * Original ISO documentation publically available at http://standards.iso.org/ittf/PubliclyAvailableStandards/index.html
+ * ISO/IEC 14496-12:2015
+ */
+package com.drew.metadata.mp4.boxes;
