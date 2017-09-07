82bd24d1843258119426aa255c88c9a21e5b4027
Drew Noakes <git@drewnoakes.com>
2017-09-07 16:07:49 +0100

    Add some more file types to ProcessAllImagesInFolderUtility

--- Source/com/drew/tools/ProcessAllImagesInFolderUtility.java
+++ Source/com/drew/tools/ProcessAllImagesInFolderUtility.java
@@ -174,7 +174,9 @@ public class ProcessAllImagesInFolderUtility
             Arrays.asList(
                 "jpg", "jpeg", "png", "gif", "bmp", "ico", "webp", "pcx", "ai", "eps",
                 "nef", "crw", "cr2", "orf", "arw", "raf", "srw", "x3f", "rw2", "rwl",
-                "tif", "tiff", "psd", "dng"));
+                "tif", "tiff", "psd", "dng",
+                "3g2", "3gp", "m4v", "mov", "mp4",
+                "pbm", "pnm", "pgm"));
 
         private int _processedFileCount = 0;
         private int _exceptionCount = 0;
