bc494cef193bb6ad6173fd589c9ae65b97ed72f0
Drew Noakes <git@drewnoakes.com>
2017-09-08 11:18:09 +0100

    Remove unused methods

--- Source/com/drew/lang/ByteUtil.java
+++ Source/com/drew/lang/ByteUtil.java
@@ -57,34 +57,4 @@ public class ByteUtil
                 ((buffer[offset + 7] & 0xFF) << 56);
         }
     }
-
-    public static long getUnsignedInt32(byte[] buffer, int offset, boolean bigEndian)
-    {
-        int firstByte = (0x000000FF & ((int)buffer[offset]));
-        int secondByte = (0x000000FF & ((int)buffer[offset+1]));
-        int thirdByte = (0x000000FF & ((int)buffer[offset+2]));
-        int fourthByte = (0x000000FF & ((int)buffer[offset+3]));
-
-        return ((long) (firstByte << 24
-            | secondByte << 16
-            | thirdByte << 8
-            | fourthByte))
-            & 0xFFFFFFFFL;
-
-    }
-
-    public static long getLong32(byte[] buffer, int offset, boolean bigEndian)
-    {
-        if (bigEndian) {
-            return (buffer[offset] << 24 & 0xFF000000) |
-                (buffer[offset + 1] << 16 & 0xFF0000) |
-                (buffer[offset + 2] << 8 & 0xFF00) |
-                (buffer[offset + 3] & 0xFF);
-        } else {
-            return (buffer[offset + 3] << 24 & 0xFF000000) |
-                (buffer[offset + 2] << 16 & 0xFF0000) |
-                (buffer[offset + 1] << 8 & 0xFF00) |
-                (buffer[offset] & 0xFF);
-        }
-    }
 }
