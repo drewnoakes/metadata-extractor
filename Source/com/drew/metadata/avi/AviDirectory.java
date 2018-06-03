5e666c13d1ffbd235e354dc32431e99648b7f3f9
PerBalling <PerBalling@users.noreply.github.com>
2018-06-03 09:07:25 +0200

    Support for DateTimeOriginal
    Reference:
    https://www.sno.phy.queensu.ca/~phil/exiftool/TagNames/RIFF.html
    RIFF BroadcastExt Tags
    320     DateTimeOriginal
    
    This tag is valid for at least two camera models:
    Canon Canon PowerShot SX30 IS
    Canon Canon DIGITAL IXUS 970

--- Source/com/drew/metadata/avi/AviDirectory.java
+++ Source/com/drew/metadata/avi/AviDirectory.java
@@ -41,6 +41,7 @@ public class AviDirectory extends Directory
     public static final int TAG_WIDTH = 6;
     public static final int TAG_HEIGHT = 7;
     public static final int TAG_STREAMS = 8;
+    public static final int TAG_DATETIME_ORIGINAL = 320;
 
     public static final String CHUNK_STREAM_HEADER = "strh";
     public static final String CHUNK_MAIN_HEADER = "avih";
@@ -62,6 +63,7 @@ public class AviDirectory extends Directory
         _tagNameMap.put(TAG_WIDTH, "Width");
         _tagNameMap.put(TAG_HEIGHT, "Height");
         _tagNameMap.put(TAG_STREAMS, "Stream Count");
+        _tagNameMap.put(TAG_DATETIME_ORIGINAL, "DateTime Original");
     }
 
     public AviDirectory()
