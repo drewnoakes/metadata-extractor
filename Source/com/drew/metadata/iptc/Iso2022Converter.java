d8de9735dd1523fd0397dad5482396b62e4aeefa
Drew Noakes <git@drewnoakes.com>
2019-07-06 23:12:23 +1000

    Bump copyright year

--- Source/com/drew/metadata/iptc/Iso2022Converter.java
+++ Source/com/drew/metadata/iptc/Iso2022Converter.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2017 Drew Noakes
+ * Copyright 2002-2019 Drew Noakes and contributors
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
