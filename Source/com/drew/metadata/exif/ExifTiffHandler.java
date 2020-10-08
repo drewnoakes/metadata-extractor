3d21ad9b219498ef9be10354591f5e8b93ce5743
dohykim <dohykim1@gmail.com>
2020-10-08 19:41:49 +0900

    Fix processMakernote cannot lowerCase Samsung maker (#506)
    Fix processMakernote cannot lowerCase Samsung maker
    
    https://github.com/drewnoakes/metadata-extractor/issues/505

--- Source/com/drew/metadata/exif/ExifTiffHandler.java
+++ Source/com/drew/metadata/exif/ExifTiffHandler.java
@@ -652,7 +652,7 @@ public class ExifTiffHandler extends DirectoryTiffHandler
             ReconyxUltraFireMakernoteDirectory directory = new ReconyxUltraFireMakernoteDirectory();
             _metadata.addDirectory(directory);
             processReconyxUltraFireMakernote(directory, makernoteOffset, reader);
-        } else if ("SAMSUNG".equals(cameraMake)) {
+        } else if ("SAMSUNG".equalsIgnoreCase(cameraMake)) {
             // Only handles Type2 notes correctly. Others aren't implemented, and it's complex to determine which ones to use
             pushDirectory(SamsungType2MakernoteDirectory.class);
             TiffReader.processIfd(this, reader, processedIfdOffsets, makernoteOffset, tiffHeaderOffset);
