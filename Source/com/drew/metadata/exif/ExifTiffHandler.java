c594940e90334cc7484d3807a573fa34ca271c1a
Roel van Dijk <roel@rustradio.org>
2021-05-21 14:43:58 +0200

    Fix missing import.

--- Source/com/drew/metadata/exif/ExifTiffHandler.java
+++ Source/com/drew/metadata/exif/ExifTiffHandler.java
@@ -24,6 +24,7 @@ import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
 
