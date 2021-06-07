d6d08d0fc92597dc9cd081bdb5195dea5015996f
Drew Noakes <git@drewnoakes.com>
2021-06-07 12:14:49 +1000

    Reinstate validation

--- Source/com/drew/metadata/plist/BplistReader.java
+++ Source/com/drew/metadata/plist/BplistReader.java
@@ -29,7 +29,6 @@ import com.drew.lang.annotations.Nullable;
 /**
  * A limited-functionality binary property list (BPLIST) utility.
  * Parser functionality accounts for &quot;dict&quot; (with simple integer and string values) and &quot;data&quot;.
- * <b>NOTE:</b> Callers should call the {@link #isValid(byte[])} method before attempting to parse.
  *
  * https://opensource.apple.com/source/CF/CF-550/ForFoundationOnly.h
  * https://opensource.apple.com/source/CF/CF-550/CFBinaryPList.c
@@ -65,6 +64,10 @@ public class BplistReader
 
     public static PropertyListResults parse(byte[] bplist) throws IOException
     {
+        if (!isValid(bplist)) {
+            throw new IllegalArgumentException("Input is not a bplist");
+        }
+
         final ArrayList<Object> objects = new ArrayList<Object>();
         final Trailer trailer = readTrailer(bplist);
 
