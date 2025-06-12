7d7adb00df8057a4806dda7d7d91c7341d782d2b
leviZeng <levi.zeng@hotmail.com>
2025-06-12 09:46:27 +0800

    chore(license): standardize license file to match repo requirements
    Updated the license file in this directory to be consistent with the rest of the repository.
    Also aligned the license in the other directory file as noted.

--- Source/com/drew/metadata/exif/makernotes/DjiMakernoteDescriptor.java
+++ Source/com/drew/metadata/exif/makernotes/DjiMakernoteDescriptor.java
@@ -1,5 +1,22 @@
 /*
- *Copyright (c) Drew Noakes and contributors. All Rights Reserved. Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
+ * Copyright 2002-2019 Drew Noakes and contributors
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ * More information about this project is available at:
+ *
+ *    https://drewnoakes.com/code/exif/
+ *    https://github.com/drewnoakes/metadata-extractor
  */
 package com.drew.metadata.exif.makernotes;
 
@@ -8,11 +25,12 @@ import com.drew.lang.annotations.Nullable;
 import com.drew.metadata.TagDescriptor;
 
 /**
- *  <summary>
- *  Provides human-readable string representations of tag values stored in a <see cref="DjiMakernoteDirectory"/>.
- *  </summary>
- *  <remarks>Using information from https://metacpan.org/pod/distribution/Image-ExifTool/lib/Image/ExifTool/TagNames.pod#DJI-Tags</remarks>
- *  <author>SeanJay Zeng, adapted from Drew Noakes https://drewnoakes.com</author>
+ * Provides human-readable string representations of tag values stored in a {@link DjiMakernoteDirectory}.
+ * <p>
+ * Using information from <a href="https://metacpan.org/pod/distribution/Image-ExifTool/lib/Image/ExifTool/TagNames.pod#DJI-Tags">DJI Tags Documentation</a>.
+ *
+ * @author SeanJay Zeng
+ * @author Adapted from Drew Noakes (https://drewnoakes.com)
  */
 @SuppressWarnings("WeakerAccess")
 public class DjiMakernoteDescriptor extends TagDescriptor<DjiMakernoteDirectory>
